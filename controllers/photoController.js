16eead08eed9aaf4545c4e1e8187eaaf3e970521
202010225037 <202010225037@mhs.ubharajaya.ac.id>
2023-04-15 12:07:10 +0700

    update testing

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,86 +1,82 @@
 const { Photo, User } = require("../models")
 
 class PhotoController {
-    static getAllPhotos(req, res) {
-        Photo.findAll({
-            include: User
-        })
-        .then(result => {
-            res.status(200).json(result)
-        })
-        .catch(error => {
-            res.status(404).json(error)
-        })
-    }
-    static getOnePhotoByID(req, res) {
-        let id = req.params.id
-        Photo.findByPk(id)
-        .then(result => {
-            res.status(201).json(result)
-        })
-        .catch(error => {
-            res.status(405).json(error)
-        })
+  static getAllPhotos(req, res) {
+    Photo.findAll({
+      include: User,
+    })
+      .then((result) => {
+        res.status(200).json(result)
+      })
+      .catch((error) => {
+        res.status(404).json(error)
+      })
+  }
+  static getOnePhotoByID(req, res) {
+    let id = req.params.id
+    Photo.findByPk(id)
+      .then((result) => {
+        res.status(201).json(result)
+      })
+      .catch((error) => {
+        res.status(405).json(error)
+      })
+  }
+  static createPhoto(req, res) {
+    const { title, caption, image_url } = req.body
+    //console.log(res.locals.user);
+    Photo.create({
+      title,
+      caption,
+      image_url,
+      UserId: res.locals.user.id,
+    })
 
-    }
-    static createPhoto(req, res) {
-        const { title, caption, image_url } = req.body
-        //console.log(res.locals.user);
-        Photo.create({
-            title,
-            caption,
-            image_url,
-            UserId: res.locals.user.id  
-        })
+      .then((result) => {
+        res.status(202).json(result)
+      })
+      .catch((error) => {
+        res.status(406).json(error)
+      })
+  }
+  static updateOnePhotoByID(req, res) {
+    let id = req.params.id
 
-        .then(result => {
-            res.status(202).json(result)
-        })
-        .catch(error => {
-            res.status(406).json(error)
-        })
+    const { title, caption, image_url } = req.body
+    let data = {
+      title,
+      caption,
+      image_url,
     }
-    static updateOnePhotoByID(req, res) {
-        let id = req.params.id
 
-        const { title, caption, image_url } = req.body
-        let data = {
-            title, 
-            caption, 
-            image_url
-        }
-        
-        Photo.update(
-            data, 
-            {
-                where: {
-                    id
-                },
-                returning: true
-            }
-        )
-        .then(result => {
-            res.status(207).json(result)
-        })
-        .catch(error => {
-            res.status(407).json(error)
-        })
-    }
-    static deleteOnePhotoByID(req, res) {
-        let id = req.params.id
+    Photo.update(data, {
+      where: {
+        id,
+      },
+      returning: true,
+    })
+      .then((result) => {
+        res.status(207).json(result)
+      })
+      .catch((error) => {
+        res.status(407).json(error)
+      })
+  }
+  static deleteOnePhotoByID(req, res) {
+    let id = req.params.id
 
-        Photo.destroy({
-            where: {
-                id
-            }
-        })
-        .then(result => {
-            res.status(208).json(result)
-        })
-        .catch(error => {
-            res.status(408).json(error)
-        })
-    }
+    Photo.destroy({
+      where: {
+        id,
+      },
+    })
+      .then((result) => {
+        res.status(208).json(result)
+      })
+      .catch((error) => {
+        res.status(408).json(error)
+      })
+  }
 }
 
 module.exports = PhotoController
